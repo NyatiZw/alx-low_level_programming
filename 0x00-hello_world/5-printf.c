8f3783a3b66906df296e8819dfaeb00604bf69c2
Simba Hanga <appsmaid19@gmail.com>
2022-12-08 21:06:53 +0200

    betty correctons

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,17 @@
 # include <stdio.h>
 
-int main (void){
+/**
+ * main - Entry point
+ *
+ * Description: 'using printf function'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
 
 	printf("with proper grammer, but the outcome is a piece of art,\n");
 
-		return 0;
+		return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,19 @@
 # include <stdio.h>
 
-int main(void){
+/**
+ * main -Entry point
+ *
+ * Description: 'checking sizeof'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
 	printf("Size of a : "\n, char);
 	printf("Size of an : "\n, int);
 	printf("Size of a : "\n, long int);
 	printf("Size of a: "\n, float);
-
 }
 
-return 0 ;
+return (0);
