7cb19261d9021f18f3ce867ff12b357cf46ec920
Simba Hanga <appsmaid19@gmail.com>
2022-12-08 20:55:37 +0200

    initial commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+
+int main(void){
+	printf("Size of a : "\n, char);
+	printf("Size of an : "\n, int);
+	printf("Size of a : "\n, long int);
+	printf("Size of a: "\n, float);
+
+}
+
+return 0 ;
