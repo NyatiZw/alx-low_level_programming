9d727b2f532bd15e0731774c6524f172cf38bb75
NyatiZw <appsmaid19@gmail.com>
2022-12-08 15:50:35 -0800

    hot fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -11,7 +11,8 @@
 int main(void)
 {
 
-	puts("\"Programming is like building a multilingual puzzle\n");
+	fputs("\"Programming is like building a multilingual puzzle\n", stdout);
+	fputs("%s\n", stderr);
 
 	return (0);
 }
