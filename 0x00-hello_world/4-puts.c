d797733cc6f888928a2c2eb5e35350f8950913fb
NyatiZw <appsmaid19@gmail.com>
2022-12-08 15:19:02 -0800

    checking syntax

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -11,7 +11,7 @@
 int main(void)
 {
 
-	puts("Programming is like building a multilingual puzzle\n");
+	fputs("Programming is like building a multilingual puzzle\n");
 	fputs(stderr);
 
 	return (0);
