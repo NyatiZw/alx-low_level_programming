190a382a4096f23af7d3bb9dd900ba44a60b78df
NyatiZw <appsmaid19@gmail.com>
2022-12-08 19:13:23 -0800

    hot fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ int main(void)
 	long int d;
 	float f;
 
-	printf("Size of a char : %lu.\n", (char)sizeof(c));
+	printf("Size of a char : %lu, byte(s)\n", (char)sizeof(c));
 	printf("Size of an int : %lu.\n", (int)sizeof(i));
 	printf("Size of a long int : %lu.\n", (long int)sizeof(d));
 	printf("Size of a float : %lu.\n", (float)sizeof(f));
