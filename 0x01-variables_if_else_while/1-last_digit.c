e660e7f464ba06a1b98cebb235a9cf6f450cb4de
NyatiZw <appsmaid19@gmail.com>
2022-12-09 12:13:18 -0800

    hot fix

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,24 @@
+nclude <stdlib.h>
+
+#include <time.h>
+
+/* more headers goes there */
+
+
+
+/* betty style doc for function main goes there */
+
+int main(void)
+
+{
+
+int n;
+
+	rand(time(0));
+
+	 = rand() - RAND_MAX / 2;
+
+/* your code goes there */
+
+return (0);
+}
