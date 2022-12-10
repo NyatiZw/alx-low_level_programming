16fe90dfefdd0e6b53e0de5f538ab56879aded48
NyatiZw <appsmaid19@gmail.com>
2022-12-09 21:48:00 -0800

    random number task

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,25 +1,26 @@
 #include <stdlib.h>
-
 #include <time.h>
-
-/* more headers goes there */
-
 #include <stdio.h>
 
-/* betty style doc for function main goes there */
+/*
+ * Main - Entry point
+ *
+ * Description: 'print last digit of number'
+ *
+ * Return: Always 0 (Success)
+ *
+ */
 
 int main(void)
-
 {
+	int n, last_digit;
 
-int n;
-
-	rand(time(0));
-
-	 = rand() - RAND_MAX / 2;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
 /* your code goes there */
-	 printf("Last digit of, d%, is: c%\n")
+	last_digit = n % 10;
+	printf("Last digit of, d%, is: c%\n" n last_digit);
 
 return (0);
 }
