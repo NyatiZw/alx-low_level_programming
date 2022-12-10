d446ee4e5819074e853d6ae7c79b48d15c17670a
NyatiZw <appsmaid19@gmail.com>
2022-12-10 07:11:51 -0800

    using putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - entry point
+ *
+ * Description: 'The Alphabet game'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	for(int i = 'A'; i <= 'Z'; i++)
+	{
+		int lower_i = tolower(i);
+		putchar(lower_i);
+	}
+
+	return (0);
+}
