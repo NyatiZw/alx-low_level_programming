6fb0eaa5552353be596c678c4f2ab1cb99d64d47
NyatiZw <appsmaid19@gmail.com>
2022-12-10 16:46:41 -0800

    task on removing char from str

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,25 @@
+# include <stdio.h>
+# include <ctype.h>
+/**
+ * main - Entry Point
+ *
+ * Description: 'C variables loops'
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void) 
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+		putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
