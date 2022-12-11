9c0ab4ad97b505062984a21651474f3e9dd37051
NyatiZw <appsmaid19@gmail.com>
2022-12-11 02:29:41 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+# include <stdio.h>
+# include <ctype.h>
+# include <string.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: 'While function'
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch='0x0'; ch<='0xf'; ch++)
+		putchar(ch);
+	putchar('\n');
+
+	return (0);
+}
