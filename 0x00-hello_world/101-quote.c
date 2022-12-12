148c4595fb26ff7bdde5a1d0c54de6eec1779e5f
NyatiZw <appsmaid19@gmail.com>
2022-12-12 10:51:05 -0800

    Betty styling

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,14 @@
 # include <stdio.h>
 
+/**
+ * main - Entry Point
+ *
+ * Description: 'returning a false value'
+ *
+ * Return: 1 (False)
+ *
+ */
+
 int main(void)
 {
 	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
