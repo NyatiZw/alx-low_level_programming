44607125bea6e890bea0b0a9aec1d1db17d98857
NyatiZw <appsmaid19@gmail.com>
2022-12-14 09:54:28 -0800

    header files

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+# include "main.h"
+
+/**
+ * int _abs(int) -entry point
+ *
+ * Description:- 'function to compute the absolute value of an integer'
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int _abs(int)
+{
+	int c = int, n;
+
+	n = _abs(c);
+
+	_putchar(n);
+	_putchar('\n');
+
+	return (0);
+}
