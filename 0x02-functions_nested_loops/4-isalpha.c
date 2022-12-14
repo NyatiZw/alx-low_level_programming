65c901db155711bd63fe9289395c58dbb51ed96c
NyatiZw <appsmaid19@gmail.com>
2022-12-14 05:19:57 -0800

    hot fix

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+# include "main.h"
+
+/** int isalpha: entry point
+ *
+ * Description:- 'Function int _isalpha to check code'
+ *
+ * Return: 1
+ *
+ */
+
+int isaplha(void)
+{
+	int c;
+
+	res = _isalpha(char c);
+
+	result (0);
+}
