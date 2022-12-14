46d1bdc9923d7637f76b0ca702b12dc8f2b170fe
NyatiZw <appsmaid19@gmail.com>
2022-12-14 02:41:34 -0800

    nested functions and loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+# include "main.h"
+
+/**
+ * description: 'function _islower()'
+ *
+ * Return: 1 (Success) || 0 (Not)
+ *
+ */
+
+islower()
+
+int main(void)
+{
+	int c;
+
+	c = 'T';
+
+	if c (islower 97 && c <= 122)
+		putchar('1');
+		else
+		putchar('0');
+
+		return (1);
+}
