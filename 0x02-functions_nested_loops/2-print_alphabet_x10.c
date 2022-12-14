cb76b665fe1f6c805d900a83f550a06a3d32b72d
NyatiZw <appsmaid19@gmail.com>
2022-12-13 18:54:09 -0800

    c header files

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,23 @@
 # include "main.h"
+
+/**
+ * print_alphabetx10 - entry
+ *
+ * Description:- " c functions & nested loops"
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+void print_alphabet_x10(void)
+
+{
+	int alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+
+	{
+		putchar(alpha * 10);
+	}
+	putchar('\n');
+}
