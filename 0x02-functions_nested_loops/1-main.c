82dc67315a058b296504f6afca9778f0d1b4556c
NyatiZw <appsmaid19@gmail.com>
2022-12-13 16:59:07 -0800

    hot fix

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,22 +1,2 @@
 # include "main.h"
-
-
-
-/**
- *  main - check the code
- *
- *  Description: "nested loops and header files"
- *
- * Return: Always 0.
- *
- */
-
-int main(void)
-
-{
-
-	void print_alphabet(void);
-
-	return (0);
-
-}
+void print_alphabet(void);
