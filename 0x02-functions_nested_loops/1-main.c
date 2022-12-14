8f3354cfd46a29f391a91f640701cd789aeeeb04
NyatiZw <appsmaid19@gmail.com>
2022-12-13 17:30:19 -0800

    hot fix

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,18 +1,2 @@
 # include "main.h"
-
-/**
- * main - check the code
- *
- * Description: "check the code"
- *
- * Return: Always 0.
- */
-
-int main(void)
-
-{
-	print_alphabet();
-
-	return (0);
-
-}
+print_alphabet();
