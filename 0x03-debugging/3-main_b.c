052114418ab1ca8325bf92d552e61d8491a60f07
NyatiZw <appsmaid19@gmail.com>
2022-12-15 08:22:44 -0800

    debugging

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -10,16 +10,16 @@
 int main(void)
 {
 
-int convert_day(int, int);
+	int convert_day(int, int);
 	int month;
 	int day;
 	int year;
 
-	month = 4;
-	day = 01;
-	year = 1997;
+	month = 02;
+	day = 29;
+	year = 2000;
 
-	printf("Date : % 02d / % 02d / % 04d \n", month, day, year);
+	printf("Date : %02d/%02d/%04d \n", month, day, year);
 	day = convert_day(month, day);
 
 	print_remaining_days(month, day, year);
