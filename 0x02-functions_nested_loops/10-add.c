56d034a36a5b73ed02a4f85312fefa2d464f19d3
NyatiZw <appsmaid19@gmail.com>
2022-12-14 19:10:26 -0800

    add function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,22 @@
+# include "main.h"
+
+/**
+ * add - function to display times table
+ *
+ * Return: times table
+ *
+ */
+
+
+
+int add(int, int)
+
+{
+	int a, b;
+
+	int res;
+
+	res = a +b;
+
+	return(res);
+}
