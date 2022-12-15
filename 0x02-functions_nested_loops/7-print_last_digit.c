4bf3d984a330cdcc2b14aef9b10e03a5c2fd5685
NyatiZw <appsmaid19@gmail.com>
2022-12-14 16:12:49 -0800

    hot fix

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+# include "main.h"
+
+/**
+ * print_last_digit - entry point
+ *
+ * Description:- " c functions & nested loops"
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int print_last_digit(int)
+{
+	int ld = 0, n;
+
+	ld = n % 10;
+
+	if (ld == 0)
+		_putchar(n);
+	else if (ld > 5)
+		_putchar(ld);
+	else if (ld <= 6 && 10)
+		_putchar(ld);
+	return 0;
+}
