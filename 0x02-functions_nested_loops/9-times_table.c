eab261a93c2c904cfa7348b9ec978420ad4c7313
NyatiZw <appsmaid19@gmail.com>
2022-12-14 19:52:54 -0800

    hot fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,7 +12,7 @@
 void times_table(void)
 
 {
-	int r,c,d;
+	int r, c, d;
 
 	for (r = 0; r <= 9; r++)
 	{
@@ -22,7 +22,7 @@ void times_table(void)
 		for (c = 1; c <= 9; c++)
 		{
 			d = (r * c);
-			if ((d / 10) > 0 )
+				if ((d / 10) > 0)
 				{
 					_putchar((d / 10) + '0');
 				}
