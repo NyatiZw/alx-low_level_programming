1f4cc78be5f5a34d45444e42bb277540bd5e2931
NyatiZw <appsmaid19@gmail.com>
2022-12-16 07:41:16 -0800

    hot fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,12 +10,15 @@ void more_numbers(void)
 {
 	int i;
 	int j;
-	
-	for (i = 0; i <= 14; _putchar(" %d", i++));
-	
-	while (i <= 14) 
+
+	for (i = 0; i <= 14;)
+		_putchar("%d", i);
+		i++;
+
+	while (i <= 14)
 	{
-		for (j = 0; j <= 10; _putchar("\n"));
+		for (j = 0; j <= 10;)
+		_putchar("\n");
 		j++;
 	}
 	return (0);
