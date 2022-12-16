1c86bd35fd4e9e9ad598a03c3e395cc176b2cd64
NyatiZw <appsmaid19@gmail.com>
2022-12-16 02:30:32 -0800

    nested loops

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+# include "main.h"
+/**
+ * print_most_numbers- function to print numbers
+ *
+ * Return: void
+ */
+
+
+void print_most_numbers(void)
+{
+	int a = 0;
+
+	while (a <= 10)
+	{
+		{
+			if (a != 2 && a != 4)
+				_putchar(a);
+		}
+		a++;
+	}
+
+	_putchar('\n');
+}
+
