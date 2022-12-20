35bec4cddb0dd5756a6ffa891964fb7dc724c223
NyatiZw <appsmaid19@gmail.com>
2022-12-20 14:15:10 -0800

    pointers task arrays and strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * reset_to_98- Entry point
+ *
+ * Description:- 'function that updates the value it points to to 98'
+ * @n:- reset to integer parameter
+ *
+ * Return: Void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+	_putchar(*n);
+	_putchar('\n');
+}
+
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Alx task on pointers, arrays and string
