b8714bf5bf25a86ddd639b1554d67fd4b92c3cce
NyatiZw <appsmaid19@gmail.com>
2022-12-20 15:37:25 -0800

    hot fix

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - Entry point
+ *
+ * Description:- 'function that swaps the values of two integers'
+ * @a:- integer parameter
+ * @b:- integer parameter
+ *
+ * Return: Void
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = &b;
+	_putchar(*a);
+}
