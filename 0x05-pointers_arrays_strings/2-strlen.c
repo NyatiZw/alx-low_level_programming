6157c57c57ed9fc8b0115ed5b2bc6b36f24b18dd
NyatiZw <appsmaid19@gmail.com>
2022-12-20 17:08:38 -0800

    strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - Entry point
+ *
+ * Description:- 'function that returns the lenght of a string'
+ * @s:- char pointer
+ *
+ * Return: Void
+ */
+
+int _strlen(char *s)
+{
+	char *s[];
+}
