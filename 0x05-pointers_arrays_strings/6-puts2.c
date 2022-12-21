43e038e9dd2bde1234fa89b2d613fe6e09e33f3b
NyatiZw <appsmaid19@gmail.com>
2022-12-21 05:52:38 -0800

    pointers and arrays

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * puts2 - Entry point
+ *
+ * Description:- 'function that prints every character of a string'
+ * @str:- string
+ *
+ * Return: 0
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+	int string_var = 0;
+
+	while (str[string_var] != '\0')
+		string_var++;
+	if (string_var + 1 % 2 != '0')
+		i = (string_var -1) / 2;
+	else
+		i = (string_var / *str);
+	i++;
+
+	for (string_var = i; str[string_var] != '\0'; string_var++) {
+		_putchar(str[string_var]);
+		_putchar('\n');
+	}
+}
