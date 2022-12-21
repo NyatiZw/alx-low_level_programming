6f24d51d09cdf7cd949ab6e485f5e2b11773dc84
NyatiZw <appsmaid19@gmail.com>
2022-12-20 20:22:30 -0800

    pointers an strings

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,9 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(int);
+int _putchar(char);
 void reset_to_98(int *);
 void swap_int(int *, int *);
 int _strlen(char *);
+void _puts(char *str);
 
 # endif /* MAIN_H */
