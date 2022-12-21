c173792e27fb8a2a3ad655a45e84bf0fa5750881
NyatiZw <appsmaid19@gmail.com>
2022-12-21 03:09:50 -0800

    hot fix

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,21 +11,21 @@
 
 void rev_string(char *s)
 {
-	int i;
-	int j;
-	
-	int l = 0;
 
 	char *start;
 	char *end;
 	char ch;
+	int i;
+	int j;
+
+	int l = 0;
 
 	start = s;
 	end = s;
 
 	for (j = 0; s[j] != '\0'; j++)
 	{
-		for (i = 0; i < (l - 1) / 2; i--)
+		for (i = 0; i < (l - 1) / 2; i++)
 		{
 			ch = *end;
 			*end = *start;
