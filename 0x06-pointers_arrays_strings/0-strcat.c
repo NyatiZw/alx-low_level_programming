972b38b565ae1e3fb987557f133259b07bd350ca
NyatiZw <appsmaid19@gmail.com>
2022-12-22 10:15:10 -0800

    strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,8 +5,8 @@
  * _strcat- function to concatenate two strings
  *
  * Description: 'function that concatenates two strings'
- * @dest: destination string
- * @src: source string
+ * @str1: destination string
+ * @str2: source string
  *
  * Return: 0 (Success)
  */
--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "ctype.h"
 
 /**
  * _strcat- function to concatenate two strings
@@ -10,34 +11,25 @@
  * Return: 0 (Success)
  */
 
-char *_strcat(char *dest, char *src)
+char *_strcat(char *str1, char *str2)
 {
-	char str1[100];
-	char str2[100];
-	char temp[100];
-
 	int i = 0, j = 0;
-
-	str1[100] = *dest;
-	str2[100] = *src;
 
 	while (str1[i] != '\0')
 	{
-		temp[j] = str1[i];
+		++str1;
 		i++;
-		j++;
 	}
 
-	i = 0;
-	while (str2[i] != '\0')
+	while (str2[j] != '\0')
 	{
-		temp[j] = str2[i];
-		i++;
+		*str1 = *str2;
+		str1++;
+		str2++;
 		j++;
 	}
-	temp[j] = '\0';
 
-	_putchar("%s", temp);
+	_putchar(*str1);
 
 	return (0);
 }
