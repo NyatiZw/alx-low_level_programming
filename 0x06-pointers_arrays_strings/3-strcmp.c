cde13dda32749392be74414eadc2f45663c025ec
NyatiZw <appsmaid19@gmail.com>
2022-12-22 15:52:06 -0800

    initial commit

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcmp- Entry Point
+ *
+ * Description:- ' Function that compares two strings
+ * @s1: string destination
+ * @s2: string source
+ *
+ * Return: 0 (Success)
+ */
+
+int *_strcmp(char *s1, char *s2)
+{
+	int i, j;
+
+	for (i = 0; str1[i] != '\0'; i++)
+	{
+		++str1;
+	}
+	for (j = 0; str2[j] != '\0' && j < n; j++)
+	{
+		str1[i + j] = str2[j];
+	}
+	str1[i + j] = '\0';
+
+	return (0);
+}
