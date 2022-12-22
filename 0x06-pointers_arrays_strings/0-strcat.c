f1c76cc4387be74bc92cfa3f9543442fc3002417
NyatiZw <appsmaid19@gmail.com>
2022-12-22 05:01:18 -0800

    pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -16,14 +16,14 @@ char *_strcat(char *dest, char *src)
 	int i = 0;
 	int j = 0;
 
-	while (str1[i] != '\0')
-		i++;
-	while (str2[j] != '\0')
-	{
-		str1[i] = str2[j];
-		i++;
-		j++;
+	while (str1[i] != '\0') {
+		while (str2[j] != '\0') {
+			str1[i] = str2[j];
+			i++;
+			j++;
+		}
+		str1[i] = '\0';
+		_putchar("%s", str1);
 	}
-	str1[i] = '\0';
-	_putchar("%s", str1);
+	return (str1);
 }
