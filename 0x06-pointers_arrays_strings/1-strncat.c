e3089d918ab783abd4931a228d6a9e6f49e1fbf0
NyatiZw <appsmaid19@gmail.com>
2022-12-22 12:30:56 -0800

    strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -18,18 +18,16 @@ char *_strncat(char *str1, char *str2, int n)
 
 	while (str1[i] != '\0')
 	{
-		++str1;
 		i++;
 	}
 
 	while (str2[j] != '\0')
 	{
-		*str1 = *str2;
-		str1++;
-		str2++;
 		j++;
+		i++;
 	}
 
+	str1[i]=str2[j];
 	_putchar(*str1);
 
 	return (0);
