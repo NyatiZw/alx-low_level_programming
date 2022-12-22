ce0933780979e3ea02ef9b38cf7a48780093f36b
NyatiZw <appsmaid19@gmail.com>
2022-12-22 02:58:47 -0800

    initial commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_strcat: Entry Point
+ *
+ * Description:- 'function that concatenates to strings'
+ *
+ * Return: 0 (Success)
+ */
+
+*_strcat(char *dest, char *src)
+{
+	char str1[], str2[];
+	int i = 0;
+	int j = 0;
+
+	while(str1[i] != '\0')
+		i++;
+	while(str2[j] != '\0')
+	{
+		str1[i] = str2[j];
+		i++;
+		j++;
+	}
+	str1[i] = '\0';
+	_putchar("%s", str1);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+char *_strcat(char *, char *);
+int _putchar(char);
+
+#endif /* _MAIN_H */
