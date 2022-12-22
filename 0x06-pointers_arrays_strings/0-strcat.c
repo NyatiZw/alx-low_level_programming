9784bb9f30831b8ea9874e6b7ed23f9b23e10620
NyatiZw <appsmaid19@gmail.com>
2022-12-22 14:39:37 -0800

    strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,20 +12,23 @@
 
 char *_strcat(char *str1, char *str2)
 {
+	int i = 0, j;
+
 	while (*str1[i] != '\0')
 	{
 		str1++;
+		i++;
 	}
 
 	while (*str2[j] != '\0')
 	{
 		*str1 = *str2;
-		str1++;
-		str2++;
+		i++;
+		j++;
 	}
 
 	*str1 = '\0';
-	_putchar(a);
+	_putchar(str1);
 	
 	return (str1);
 }
