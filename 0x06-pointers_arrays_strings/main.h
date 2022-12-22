37e9de249c3151d9fe1579c230d58d09323edb46
NyatiZw <appsmaid19@gmail.com>
2022-12-22 15:26:15 -0800

    concatenated strings

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,6 +3,6 @@
 
 int _putchar(char);
 char *_strcat(char *, char *);
-char *_strncat(char *dest, char *src, int n);
-
+char *_strncat(char *, char *, int);
+char *_strncpy(char *, char *, int);
 #endif /* _MAIN_H */
