b147628337115dcce26804dc700da710b4821372
NyatiZw <appsmaid19@gmail.com>
2022-12-24 12:04:53 -0800

    hot fix

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,7 +19,7 @@ int main(void)
 		{
 			for (i = 0; i < 9; i++)
 			{
-				for (j = i + 2; j < 9; j++)
+				for (j = i + 1; j < 9; j++)
 				{
 					putchar((a % 10) + '0');
 					putchar((b % 10) + '0');
