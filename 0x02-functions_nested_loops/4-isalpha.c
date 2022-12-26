d901bc0544335f986e26c8c6254d386ddfd8b9d7
NyatiZw <appsmaid19@gmail.com>
2022-12-26 05:00:46 -0800

    corrections

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,16 +2,16 @@
 #include <ctype.h>
 
 /**
- * _islower - entry point
+ * _isalpha - entry point
  *
- * Description:- 'function to check character to lowercase'
+ * Description:- 'function to check character is aplhabetic'
  * @c:- 'character to check'
  *
  * Return: 1 (Success)
  *
  */
 
-int _islower(int c)
+int _isalpha(int c)
 {
 	if (c >= aA && c <= zZ)
 		return (1);
