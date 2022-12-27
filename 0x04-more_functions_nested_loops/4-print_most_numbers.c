a4194c9ee483bd81d82d47dbaa52b5fe57d7fd69
NyatiZw <appsmaid19@gmail.com>
2022-12-27 06:58:10 -0800

    corrections

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -13,13 +13,13 @@
 void print_most_numbers(void)
 {
 	int i;
-	char x = 'x';
+	int x = x;
 
 	for (i = 0; i <= 9;)
 	{
 		if (i == 2 || i == 4)
 		{
-			_putchar('x');
+			_putchar(x);
 		}
 	_putchar("i\n");
 	}
