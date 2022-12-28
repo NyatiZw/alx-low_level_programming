9be945d671ced95fd58f861dc01868bcf54d2df6
NyatiZw <appsmaid19@gmail.com>
2022-12-27 21:52:54 -0800

    corrections

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,16 +11,14 @@
 
 void print_diagonal(int n)
 {
-	n = '92';
+	int n = '92';
 
-	while (n == '92'){
-		for (n = '92'; n <= '92';)
+	for (n = '92'; n < '92';)
+	{
+		if (n = 0)
 		{
-			if (n = 0)
-			{
-				_putchar('\n');
-			}
+			_putchar('\n');
 		}
-		n++;
 	}
+	n++;
 }
