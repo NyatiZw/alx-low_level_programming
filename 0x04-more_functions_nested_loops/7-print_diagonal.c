a6deb07c0d2862bd401641f1bbb631a27b6948b1
NyatiZw <appsmaid19@gmail.com>
2022-12-28 23:05:53 -0800

    corrections

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,15 +12,15 @@
 void print_diagonal(int n)
 {
 	int i, k, j = 1;
-	char l = '\';
+	char sq = '#';
 
 	for (i = 1; n <= (2 * n) - 1; i++)
 	{
 		for (j = 1; j <= (2 * n) - 1; j++)
 		{
-			if (i == j || i + j == 2 * n)
+			if (i == j || i + j == (2 * n))
 			{
-				_putchar(l);
+				_putchar(sq);
 			}
 			else
 			{
