dd4f5c3d5cd98bd15bad544213e5f98fb31b8d26
NyatiZw <appsmaid19@gmail.com>
2022-12-29 09:26:46 -0800

    corrections

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,18 @@
+/**
+ * print_square- Entry point
+ *
+ * Description:- 'Function that prints a square'
+ * @size:- size of the square
+ *
+ * Return: Always (0)
+ */
+
+void print_square(int size)
+{
+	int i;
+
+	for (i = 0; i <= size; i++)
+	{
+		_putchar('#');
+	}
+}
