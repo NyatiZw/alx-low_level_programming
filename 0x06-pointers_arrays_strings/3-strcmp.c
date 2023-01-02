0dfcaba338cc8d0032b1f7d9376ebc59649c4110
NyatiZw <appsmaid19@gmail.com>
2023-01-02 06:24:23 -0800

    corrections

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,13 +10,13 @@
  * Return: s1
  */
 
-int *_strcmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
 	int i, j;
 
 	for (i = 0; str1[i] != '\0'; i++)
 	{
-		for (j = 0; str2[j] != '\0' && j < n; j++)
+		for (j = 0; (str2[j] != '\0') && (j < n); j++)
 		{
 			str1[i + j] = str2[j];
 		}
