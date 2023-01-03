8f2545dae6b7d955dbb585dda319334731e07d52
NyatiZw <appsmaid19@gmail.com>
2023-01-03 03:14:44 -0800

    task on pointers arrays and strings

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strspn- Entry point
+ *
+ * Description:- 'function that gets the length of a prefix substring'
+ * @s: string segment
+ * @accept: bytes
+ *
+ * Return: 0 (Success)
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+
+	for (i = 0; *accept++;)
+	{
+		*s = i++;
+	}
+
+	return (i);
+}
