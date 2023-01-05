02f0ae26f7a61d1ac6df3d39865467e7e7e0c8fb
NyatiZw <appsmaid19@gmail.com>
2023-01-05 02:59:33 -0800

    recursion

---
+++ 0x08-recursion.c/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion- Entry Point
+ *
+ * Description:- 'A function that prints a string'
+ * @s:- string to print
+ *
+ * Return: 0 (Success)
+ */
+
+void _puts_recursion(char *s)
+{
+	int i;
+
+	for (i = 0; i <= s[i]; i++)
+	{
+		_putchar(s[i]);
+	}
+}
