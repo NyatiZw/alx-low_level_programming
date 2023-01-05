bba3385cceb0c025b07b51d2b052b40bc8730b9a
NyatiZw <appsmaid19@gmail.com>
2023-01-05 07:28:43 -0800

    recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * is_prime_number- Entry Point
+ *
+ * Description:- 'A function that returns prime numbers'
+ * @n:- number
+ *
+ * Return: 1 (Success) 0 (Not Prime Number)
+ */
+
+int is_prime_number(int n)
+{
+	int i, m = 0, res = 0;
+
+	m = n / 2;
+
+	if (res == 0)
+	{
+		return (1);
+	}
+	if (n % i == 0)
+	{
+		res = 1;
+		return (0);
+	}
+	if (i == 2)
+	{
+		i <= m;
+		i++;
+	}
+	return (n);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,5 +8,6 @@ int _strlen_recursion(char *);
 int factorial(int);
 int _pow_recursion(int,int);
 int _sqrt_recursion(int n);
+int is_prime_number(int);
 
 #endif /* MAIN_H */
