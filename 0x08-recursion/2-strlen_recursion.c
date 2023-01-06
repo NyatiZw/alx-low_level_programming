0556e0da0ce0b9b55eb3f1ceddd97d71b2f670d3
NyatiZw <appsmaid19@gmail.com>
2023-01-06 08:08:20 -0800

    recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -13,9 +13,9 @@ int _strlen_recursion(char *s)
 {
 	int i = 0;
 
-	if (i <= s)
+	if (i <= *s)
 	{
-		return (_putchar(i++));
+		return (i++);
 	}
 	_putchar(s);
 	return (_strlen_recursion(*s + 1));
