fe84b05d88aea3439002fc3d04e0a323810d9fec
NyatiZw <appsmaid19@gmail.com>
2023-01-06 01:12:05 -0800

    recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -24,5 +24,5 @@ int _pow_recursion(int x, int y)
 	{
 		return (_pow_recursion(x * x, y / 2));
 	}
-	return (x * _pow_recursion(x * x, (y - 1) / 2));
+	return (_pow_recursion(x * x, (y - 1) / 2));
 }
