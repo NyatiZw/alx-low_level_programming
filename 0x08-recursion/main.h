ef43d538ba61fdf851e8af932e0ef3ebb1f1b288
NyatiZw <appsmaid19@gmail.com>
2023-01-06 07:51:31 -0800

    recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,7 +4,7 @@
 int _putchar(char);
 void _puts_recursion(char *);
 void _print_rev_recursion(char *);
-int _strlen_recursion(char *);
+int _strlen_recursion(char *s);
 int factorial(int);
 int _pow_recursion(int,int);
 int _sqrt_recursion(int n);
