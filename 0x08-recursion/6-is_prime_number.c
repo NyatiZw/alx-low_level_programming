aff88e2eb60d64027ece2c6333b7329f554bd4ca
NyatiZw <appsmaid19@gmail.com>
2023-01-06 02:44:58 -0800

    recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -11,15 +11,15 @@
 
 int is_prime_number(int n)
 {
-	int n = 0, res;
+	int m = 0, res;
 
-	res = n / 2;
+	m = n / 2;
 
-	if (res == 0)
+	if (m == 0)
 	{
 		return (1);
 	}
-	if (res != 0)
+	if (m % n != 0)
 	{
 		return (0);
 	}
