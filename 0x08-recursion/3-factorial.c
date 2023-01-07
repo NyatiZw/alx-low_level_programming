9bb1f9d7bd61ab18a6b996c737a934148eac7051
NyatiZw <appsmaid19@gmail.com>
2023-01-07 13:56:09 -0800

    corrections

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -11,14 +11,10 @@
 
 int factorial(int n)
 {
-	int i = 0;
-
-	if (i <= 0)
+	if (n < 0)
 	{
 		return (-1);
 	}
-	i = factorial(n - 1);
 	_putchar('n');
-
-	return (n);
+	return(n = factorial(n - 1));
 }
