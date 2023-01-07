654604265353acab0c5347e7c2c1e0194aa02cb4
NyatiZw <appsmaid19@gmail.com>
2023-01-07 07:10:09 -0800

    corrections

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -19,11 +19,11 @@ void print_most_numbers(void)
 	{
 		for (i = 0; i <= 9; i++)
 		{
-			_putchar(i);
-		}
-		if (i == 2 || i == 4)
-		{
-			_putchar('\0');
+			_putchar(i);	
+			if (i == 2 || i == 4)
+			{
+				_putchar('\0');
+			}
 		}
 	}
 }
