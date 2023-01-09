14d5b480cb48265f4016eba53b3c273d1c357e0e
NyatiZw <appsmaid19@gmail.com>
2023-01-09 12:38:01 -0800

    arguments

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -16,19 +16,19 @@ int main(int argc, char *argv[])
 {
 	int i, res = 0;
 
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
 	if (argc > 1)
 	{
 		for (i = 1; i < argc; i++)
 		{
 			res *=  atoi(argv[i]);
 			printf("%d\n", res);
-			return (0);
+			return (res);
 		}
 	}
-	else if (argc > 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	return (res);
+	return (0);
 }
