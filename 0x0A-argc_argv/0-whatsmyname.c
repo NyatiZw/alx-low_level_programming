87a625f25caeaf24ad128765b86c79c1989f088f
NyatiZw <appsmaid19@gmail.com>
2023-01-09 00:16:04 -0800

    argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main- entry point
+ *
+ * Description:- ' Function that prints it's own name'
+ *
+ * @argc:- arguments count
+ * @argv:- arguments vector
+ *
+ * Return: Always (0)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", agrv[0]);
+	return (0);
+}
