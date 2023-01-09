4be3680ed5ac303862d0fe85e1e80f9bff104356
NyatiZw <appsmaid19@gmail.com>
2023-01-09 08:42:03 -0800

    arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 	int i;
 
 	printf("argc = %d\n", argc);
-	for (i = 0; i < *argv[i]; i++)
+	for (i = 0; i < *argv[i]; i--)
 	{
 		printf("argv[%d] = %s\n", i, argv[i]);
 	}
