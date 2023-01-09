8bbe2739af8140f94b9b23cdb389d760d4ca9d6e
NyatiZw <appsmaid19@gmail.com>
2023-01-09 11:03:48 -0800

    arguments

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main- entry point
+ *
+ * Description:- ' Function that adds positive numbers'
+ *
+ * @argc:- arguments count
+ * @argv:- arguments vector
+ *
+ * Return: Always (0)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, res = 0;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			res +=  atoi(argv[i]);
+		}
+		printf("%d\n", res);
+	}
+	if (argc < 0)
+	{
+		printf("%d\n", 0);
+	}
+	return (0);
+}
