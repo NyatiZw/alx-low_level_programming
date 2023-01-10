de924915f2a63c5f2229d4ca3239a75a085f2028
NyatiZw <appsmaid19@gmail.com>
2023-01-10 09:42:05 -0800

    task on malloc and free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *a;
 
-	a = malloc((size + 1) * sizeof(char));
+	a = malloc((size) * sizeof(char));
 	if (a == NULL)
 	{
 		return (NULL);
