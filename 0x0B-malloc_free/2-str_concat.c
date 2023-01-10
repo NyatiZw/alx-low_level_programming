ebfa178c8ebe83fafcc99abaa8e54aee90b2f854
NyatiZw <appsmaid19@gmail.com>
2023-01-10 12:28:36 -0800

    task on malloc and free

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * *_strdup- Entry point
+ *
+ * Description:- 'function that returns a pointer to allocated space'
+ * @str:- string to copy
+ *
+ * Return: 0 (Success)
+ */
+
+char *_strdup(char *str)
+{
+	str = malloc(sizeof(*str));
+	return (str);
+}
