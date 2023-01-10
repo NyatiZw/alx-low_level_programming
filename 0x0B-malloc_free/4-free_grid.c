6c90831995ec458172d6e85035dbe9370cdd3421
NyatiZw <appsmaid19@gmail.com>
2023-01-10 12:50:59 -0800

    task on malloc and free

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * free_grid- Entry point
+ *
+ * Description:- 'function that frees a grid to two dimensional array'
+ * @grid:- size of array
+ * @height:- size of array
+ *
+ * Return: 0 (Success)
+ */
+
+void free_grid(int **grid, int height)
+{
+	return (0);
+}
