dd3ec8c6dad8c2d4d6e5cc8965eeae516b9611dc
NyatiZw <appsmaid19@gmail.com>
2023-01-11 07:39:29 -0800

    corrections

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -17,21 +17,25 @@ int main(int argc, char *argv[])
 {
 	int i, res = 0;
 
-	if (isdigit(argc) > 1)
+	if (!isdigit(argv[i]))
 	{
-		for (i = 1; i < argc; i++)
-		{
-			res += atoi(argv[i]);
-		}
-	}
-	if (isdigit(argc) < 1)
-	{
-		putchar ('\0');
+		printf("Error\n");
+
+		return (1);
 	}
 	else
 	{
-		printf("Error\n");
-		return (1);
+		if (argc > 1)
+		{
+			for (i = 1; i < argc; i++)
+			{
+				res += atoi(argv[i]);
+			}
+		}
+		if (argc < 1)
+		{
+			putchar ('\0');
+		}
 	}
 
 	printf("%d\n", res);
