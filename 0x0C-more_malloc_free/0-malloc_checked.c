a5f97c79ab072338531089f9b49af029b49ae4f0
NyatiZw <appsmaid19@gmail.com>
2023-01-12 00:50:20 -0800

    more malloc and free

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *malloc_checked- Entry point
+ *
+ * Description:- 'Function that allocates memory using malloc'
+ * @b:- integer value
+ *
+ * Return: 0 Success
+ */
+
+*malloc_checked(unsigned int b)
+{
+	int i;
+
+	b = malloc(sizeof(int) * i * b);
+	if (malloc == NULL)
+	{
+		exit(*malloc_checked);
+		return (98);
+	}
+	b = *b;
+	return (*b);
+}
---
+++ 0x0C-more_malloc_free/_putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _putchar- Entry point
+ *
+ * Description:- 'function to print on the stdout'
+ * @c:- character to print
+ *
+ * Return: 0 Success
+ */
+
+int _putchar(char c)
+{
+	write(1, &c, 1);
+}
