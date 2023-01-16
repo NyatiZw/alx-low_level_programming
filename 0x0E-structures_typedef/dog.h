796d2ad377c4ab610c7625c1171b7d4fb9acb215
NyatiZw <appsmaid19@gmail.com>
2023-01-16 03:07:37 -0800

    structs

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * struct dog - Entry point
+ *
+ * Description:- 'data structures'
+ * @owner:- member
+ * @name:- member
+ * @age:- member
+ *
+ * Return: 0 Success
+ */
+
 struct dog {
-	char *owner;
 	char *name;
 	int float age;
+	char *owner;
 };
