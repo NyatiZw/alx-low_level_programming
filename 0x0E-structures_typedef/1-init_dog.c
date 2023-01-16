8697584df13220edbe35451ee5f3c5c0bc98184d
NyatiZw <appsmaid19@gmail.com>
2023-01-16 14:09:23 -0800

    struct and typedef

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * init_dog- Entry Point
+ *
+ * Description:- 'function that initializes a variable'
+ * @d:- member
+ * @name:- member
+ * @age:- member
+ * @owner:- member
+ *
+ * Return: Void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	while (d != 0)
+	{
+		name-> *name = &name;
+		age->age = &age;
+		owner-> *owner = &owner;
+	}
+}
