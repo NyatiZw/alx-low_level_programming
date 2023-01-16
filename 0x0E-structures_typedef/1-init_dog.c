cb22c16668145f2bcdd4e5364fceb04a77171b17
NyatiZw <appsmaid19@gmail.com>
2023-01-16 04:40:17 -0800

    structs

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * init_dog - Entry point
+ *
+ * Description:- 'A function that initialize a variable'
+ * @d:- mamber
+ * @name:- member
+ * @age:- member
+ * @owner:- member
+ *
+ * Return: 0 Success
+ */
+
+void init_dog (struct dog *d, char *name, float age, char *owner)
+{
+	return (0);
+}
