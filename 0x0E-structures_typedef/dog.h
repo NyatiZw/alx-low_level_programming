0484390c860e7a7d81bc7e89753d02a578986d83
NyatiZw <appsmaid19@gmail.com>
2023-01-16 13:47:32 -0800

    structs and typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,17 @@
 #ifndef _DOG_H
 #define _DOG_H
 
+/**
+ * struct dog- Entry Point
+ *
+ * Description:- 'Collection of struct dog'
+ * @name:- member
+ * @age:- member
+ * @owner:- memebr
+ *
+ * Return: 0
+ */
+
 typedef struct dog
 {
 	char *name;
