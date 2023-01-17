1792a4a78da7dcc4ea48e50cee889cb04acd0f08
NyatiZw <appsmaid19@gmail.com>
2023-01-17 03:05:17 -0800

    preprocessors

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main- Entry Point
+ *
+ * Description:- 'Program to print name of source code'
+ *
+ * Return: Always 0
+ */
+
+int main()
+{
+	printf("%s\n", _FILE_);
+	return (0);
+}
