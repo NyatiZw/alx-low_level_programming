c91dcd7a36fa13c4072884c070cfe0d5ecbf6edb
NyatiZw <appsmaid19@gmail.com>
2023-01-16 17:48:25 -0800

    struct aand typedef

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+
+/**
+ * *new_dog- Entry Point
+ *
+ * Description:- 'function that creates a new dog'
+ * @name:- member
+ * @age:- member
+ * @owner:- member
+ *
+ * Return: Void
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	typedef struct dog_t;
+	func(dog_t nw_dog)
+	{
+		struct dog_t;
+		dog_t = new_dog;
+	}
+}
