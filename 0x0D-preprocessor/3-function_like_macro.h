b6acf2b6ab18ec2e698f891d66dc86525957bf9a
NyatiZw <appsmaid19@gmail.com>
2023-01-17 03:51:37 -0800

    preprocessors

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,1 +1,15 @@
-#define ABS(X) ((X) < 0 ? -(X) : (X))
+#ifndef ABS(X)
+#define ABS(X)
+
+/**
+ * ABS- 'Function that computes absolute value of a number'
+ * @X:- Member
+ * Return: 0
+ */
+
+int ABS(int X)
+{
+	return ((X < 0) ? (-X) : (X));
+}
+
+#endif /* ABS(X) */
