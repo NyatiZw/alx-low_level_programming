8b47fea9c939368f09b3867cb5365b8db1ba7934
NyatiZw <appsmaid19@gmail.com>
2023-01-18 00:54:52 -0800

    function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ * print_name- Entry point
+ *
+ * Description:- 'Function that prints a name'
+ * @name:- member
+ * @f:- member
+ *
+ * Return: Void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	char * = &n;
+	char name = &name;
+
+	f(char n);
+
+	while (n < sizeof(name))
+	{
+		_putchar(n);
+		n++;
+	}
+}
---
+++ 0x0F-function_pointers/_putchar.c
@@ -0,0 +1,16 @@
+#include "function_pointer.h"
+
+/**
+ * _putchar- Entry point
+ *
+ * Description:- "Function to print to the stream"
+ * @c:- character
+ *
+ * Return: Always 0
+ */
+
+int _putchar(char c)
+{
+	return (c);
+}
+
