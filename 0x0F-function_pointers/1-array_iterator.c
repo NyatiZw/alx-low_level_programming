498ea1fe1b753438bf8eeafd18447cf3567a7851
NyatiZw <appsmaid19@gmail.com>
2023-01-18 05:40:57 -0800

    function pointer

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator- Entry point
+ *
+ * Description:- 'Function that executes a function given as an element'
+ * @array:- member
+ * @size:- member
+ * @action:- member
+ * @int:- member
+ *
+ * Return: Void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int n;
+	
+	for (; n < sizeof(size); n++)
+	{
+		action->&n;
+	}
+}
