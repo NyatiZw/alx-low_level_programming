878002aa8c554baf8b5a870f42cadf38915a23a2
NyatiZw <appsmaid19@gmail.com>
2023-01-19 13:33:37 -0800

    variadic functions

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,49 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_all:- 'Function that prints anything'
+ * @format:- member
+ * Return: Void
+ */
+
+void print_all(const char * const format, ...)
+{
+	va-list ap;
+	char *p, *sval;
+	int ival, x;
+	float dval;
+
+	va_start(ap, fmt);
+	while (*p = fmt)
+	{
+		if (*p != p[x])
+		{
+			printf("nil");
+		}
+		switch (*++p)
+		{
+			case 'i':
+				ival = va_arg(ap, int);
+				printf("%d", ival);
+				break;
+			case 'f':
+				dval = va_arg(ap, float);
+				printf("%f", dval);
+				break;
+			case 's':
+				while (sval == va_arg(ap, char *))
+				{
+					_putchar(*sval);
+					sval++;
+				}
+				break;
+			default:
+				_putchar(c);
+				break;
+		}
+	p++;
+	}
+	va_end(ap);
+}
