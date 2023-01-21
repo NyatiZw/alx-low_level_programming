7ef225b92e7d393a0e06d9b65e2b8226f1f1d5be
NyatiZw <appsmaid19@gmail.com>
2023-01-21 05:24:50 -0800

    correcetions

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -18,19 +18,27 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i, n = 0;
 
 	p = (unsigned int *)malloc(nmemb * sizeof(unsigned int));
-	if (nmemb == 0 || size == 0)
+	if (p != 0)
 	{
-		return (NULL);
-	}
-	
-	for (i = 0; i < n; ++i)
-	{
-		p[i] = i + 1;
-	}
+		if (nmemb == 0 && size == 0)
+		{
+			return (NULL);
+		}
 
-	for (i = 0; i < n; ++i)
+		for (i = 0; i < n; ++i)
+		{
+			p[i] = i + 1;
+		}
+
+		for (i = 0; i < n; ++i)
+		{
+			_putchar(i);
+		}
+	}
+	else
 	{
-		_putchar(i);
+		exit(-1);
+		return (NULL);
 	}
 
 	free(p);
