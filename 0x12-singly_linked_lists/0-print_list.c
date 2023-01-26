26d112ee78681212d862ecc310a2dd62b82baa05
NyatiZw <appsmaid19@gmail.com>
2023-01-26 04:57:27 -0800

    task on sting links

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,13 +13,12 @@ size_t print_list(const list_t *h)
 {
 	int i;
 
+	if (h->str == NULL)
+	{
+		printf("[0] (nil)\n");
+	}
 	while (h)
 	{
-		if (h->str == NULL)
-		{
-			printf("[0] (nil)\n");
-		}
-		
 		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		i++;
