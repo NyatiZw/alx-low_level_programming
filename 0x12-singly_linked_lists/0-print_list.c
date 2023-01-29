4bd6fcbada4ca67b7869f1294dd2f8e610f60272
NyatiZw <appsmaid19@gmail.com>
2023-01-29 02:40:51 -0800

    singly linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,16 +16,17 @@ size_t print_list(const list_t *h)
 	if (h->str == NULL)
 	{
 		printf("[0] (nil)\n");
-		
-		while (h)
-		{
-			if (h->str != NULL)
-			{
-				printf("[%d] %s\n", h->len, h->str);
-			}
-			h = h->next;
-			i++;
-		}
 	}
+		
+	while (h)
+	{
+		if (h->str != NULL)
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		i++;
+	}
+	
 	return (i);
 }
