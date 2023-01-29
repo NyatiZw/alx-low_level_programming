05157cec42ddddb81779c3a6fbf6e9a15a381287
NyatiZw <appsmaid19@gmail.com>
2023-01-29 03:16:09 -0800

    singly linked lists

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,6 +1,5 @@
 #include "lists.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
  * free_list:- 'Function that frees a list_t list'
@@ -10,9 +9,14 @@
 
 void free_list(list_t *head)
 {
+	list_t *temp;
+
 	while (head)
 	{
+		temp = head->next;
+		free(head->str);
 		free(head);
+		head = temp
 	}
 }
 
