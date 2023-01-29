7636d7c57a9befda571abc661522e27d4d0bf714
NyatiZw <appsmaid19@gmail.com>
2023-01-29 02:41:33 -0800

    singly linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -17,7 +17,7 @@ size_t print_list(const list_t *h)
 	{
 		printf("[0] (nil)\n");
 	}
-		
+
 	while (h)
 	{
 		if (h->str != NULL)
@@ -27,6 +27,6 @@ size_t print_list(const list_t *h)
 		h = h->next;
 		i++;
 	}
-	
+
 	return (i);
 }
