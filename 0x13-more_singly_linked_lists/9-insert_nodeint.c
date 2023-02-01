d5be020f2c54a554ac73f417de6a80aaa21a184b
NyatiZw <appsmaid19@gmail.com>
2023-02-01 13:26:21 -0800

    singly linked lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,7 +12,7 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *ptr = head;
+	listint_t *ptr = *head;
 	listint_t *ptr2 = malloc(sizeof(listint_t));
 
 	ptr2->n = n;
@@ -27,5 +27,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	ptr2->next = ptr->next;
 	ptr->next = ptr2;
 
-	return (0);
+	return (ptr2);
 }
