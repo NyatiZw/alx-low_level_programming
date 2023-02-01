8d21fd5695f604eac1d7169a7853e6734bc5d136
NyatiZw <appsmaid19@gmail.com>
2023-02-01 12:42:31 -0800

    singly linked lists

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,7 +9,7 @@
  * Return: Always 0
  */
 
-int *get_node_at_index(listint_t *head, unsigned int index)
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int counter;
 
