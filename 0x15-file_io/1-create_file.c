5bfb6dfe4eadd5e0a632187369e3cd01a9cba079
NyatiZw <appsmaid19@gmail.com>
2023-02-09 01:10:48 -0800

    file handling

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,29 +14,29 @@ int create_file(const char *filename, char *text_content)
 	int i;
 	int r;
 	int w;
-	
+
 	buffer = malloc(sizeof(char));
 	if (filename == NULL)
 	{
 		return (-1);
 	}
-	
+
 	if (text_content == NULL)
 	{
 		return (' ');
 	}
-	
+
 	fp = open(filename, O_RDONLY || O_WRONLY || O_TRUNC, 0600);
-	
+
 	for (i = 0; i <= (text_content[i] != '\0');)
 	{
 		i++;
 	}
 	r = read(fp, buffer, text_content[i]);
 	w = write(STDOUT_FILENO, buffer, r);
-	
+
 	free(buffer);
 	close(fp);
-	
+
 	return (w);
 }
