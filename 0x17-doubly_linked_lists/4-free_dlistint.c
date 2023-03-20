5aca1cd11d3c79d2d321f624997301cfe3a9f727
NyatiZw <appsmaid19@gmail.com>
2023-03-19 18:04:47 -0700

    freeing a node

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_delistint:- 'Function that frees a node'
+ * @head:- pointer to head 
+ * Return: nothing
+ */
+void free_dlistint(dlistint_t *head)
+{
+	if (head == NULL)
+	{
+		return;
+	}
+
+	while (head->next)
+	{
+		head = head->next;
+		free(head->prev);
+	}
+	free(head);
+}
