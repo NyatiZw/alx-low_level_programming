f55e0497f35baae6a5a4cf4abc7363d20f47a986
NyatiZw <appsmaid19@gmail.com>
2023-04-08 11:28:18 -0700

    hashTables data stuctures

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+
+/**
+ * *hash_table_create - Function to create a hash table
+ *
+ * @size: size of the array
+ *
+ * Return: pointer to hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+
+	size = malloc(sizeof(unsigned long int));
+	if (size == NULL)
+	{
+		return (NULL);
+	}
+
+	return (size);
+}
