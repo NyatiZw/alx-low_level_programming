4793ad37f78a965d724bdf5c2b72997ecbd93df0
NyatiZw <appsmaid19@gmail.com>
2023-04-08 14:07:54 -0700

    hashTables data stuctures

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,12 +11,12 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 
-	hash_node_t *i;
+	hash_node_t **i;
 
-	i = malloc(sizeof(unsigned long int));
+	*i = malloc(sizeof(unsigned long int));
 	if (i != size)
 	{
 		return (NULL);
 	}
-	return (i);
+	return (size);
 }
