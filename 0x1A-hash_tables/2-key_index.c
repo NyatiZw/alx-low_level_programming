16ad008916162f2cb92094d61326b75f5a6a9329
NyatiZw <appsmaid19@gmail.com>
2023-04-09 02:09:48 -0700

    hash table - data structures

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - function that gives index of a key (hash function)
+ *
+ * @key: the key
+ * @size: size of array of hash table
+ *
+ * Return: Index of key
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int i;
+
+	key = malloc(sizeof(unsigned long int));
+	if (key == NULL)
+	{
+		return (-1);
+	}
+
+	while (hash_djb2(key))
+	{
+		for (i = 0; i <= size[key];)
+		{
+			i++;
+		}
+	}
+
+	return (i);
+}
