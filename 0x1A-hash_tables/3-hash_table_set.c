e23eac79d4f4ee0d594d81e3c0aaa5c608ca0ecb
NyatiZw <appsmaid19@gmail.com>
2023-04-10 04:31:51 -0700

    hash tables in c - adding item

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -21,7 +21,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 
-	index = key_index((const unsigned char)key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 	for (j = index; ht->array[j]; j++)
 	{
 		if (strcmp(ht->array[j]->key, key) == 0)
